9596c9f5f3966678ba0c441053e70ee2de269eb9
Yash Arora <yasharora9416@gmail.com>
2020-04-09 08:13:08 -0700

    Almost Done

---
+++ script/yash.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAU4fFWW87qCSuAWfrhO4ex4JoxYyhUmU8",
+    authDomain: "lets-get-fit-9825a.firebaseapp.com",
+    databaseURL: "https://lets-get-fit-9825a.firebaseio.com",
+    projectId: "lets-get-fit-9825a",
+    storageBucket: "lets-get-fit-9825a.appspot.com",
+    messagingSenderId: "369234695146",
+    appId: "1:369234695146:web:a66b5cc159b5e4453fd893"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
